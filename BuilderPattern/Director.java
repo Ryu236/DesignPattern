eb8a665a98e27598947fdfb9af4b88bf5a0ecf4d
Ryu236 <ryu.adv.2360@gmail.com>
2019-11-07 23:57:32 +0900

    Feat: add Builder Pattern

---
+++ BuilderPattern/Director.java
@@ -0,0 +1,22 @@
+public class Director {
+  private Builder builder;
+  public Director(Builder builder) { // Builderのサブクラスのインスタンスが与えられるので、
+    this.builder = builder; // builderフィールドに保持しておく
+  }
+  public void construct() {
+    builder.makeTitle("Greeting");
+    builder.makeSTring("Mornig to Evening");
+    builder.makeItems(new String[] {
+      "Good Mornig",
+      "Hello",
+    });
+    builder.makeString("at night");
+    builder.makeItems(new String[] {
+      "Hello",
+      "Good night",
+      "Good bye",
+    });
+    // 文書を完成させる
+    builder.close();
+  }
+}
